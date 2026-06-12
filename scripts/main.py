80686313dcf5f82cfce05fda06b1fff3e151a3b8
Justin <justinw86420@gmail.com>
2026-06-12 16:31:51 -0400

    Create dataframe with market cap data for test stocks

--- scripts/main.py
+++ scripts/main.py
@@ -1,5 +1,13 @@
-from src import stock_list, market_cap
+import pandas as pd
+from dotenv import load_dotenv
+from src import stock_list, market_cap, financial_statements
+import os
 
+load_dotenv()
+
+fmp_api_key = os.getenv('FMP_API_KEY')
+
+# Stocks that can be tested using financial modeling
 # symbols_string = ("AAPL, TSLA, AMZN, MSFT, NVDA, GOOGL, META, NFLX, JPM, V, BAC, PYPL, DIS, T, PFE, COST, INTC, "
 #                   "KO, TGT, NKE, BA, BABA, XOM, WMT, GE, CSCO, VZ, JNJ, CVX, PLTR, SQ, SHOP, SBUX, SOFI, HOOD, "
 #                   "RBLX, SNAP, AMD, UBER, FDX, ABBV, ETSY, MRNA, LMT, GM, F, LCID, CCL, DAL, UAL, AAL, TSM, SONY, ET, "
@@ -11,8 +19,16 @@ from src import stock_list, market_cap
 
 test_stocks = ['AAPL', 'TSLA', 'AMZN']
 
+market_caps_list = []
 for stock in test_stocks:
-    ticker = market_cap.get_yfinance_ticker(t=stock)
-    price_history = market_cap.yfinance_historic_price(t=ticker)
-    shares_history = market_cap.yfinance_get_shares(t=ticker)
-    mc = market_cap.calc_market_cap()
+    # Get balance sheet from financial modeling prep
+    balance_sheet = financial_statements.get_balance_sheet(t=stock, key=fmp_api_key)
+    # Calculate market cap
+    mc = market_cap.calc_market_cap(t=stock, balance=balance_sheet)
+    market_caps_list.append(mc)
+market_cap_dict = dict(zip(test_stocks, market_caps_list))
+df_market_cap = pd.DataFrame.from_dict(market_cap_dict)
+
+#
+
+
