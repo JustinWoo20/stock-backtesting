d1ecfda8314ccef2651933d59781d47b6efdc945
Justin <justinw86420@gmail.com>
2026-07-02 16:00:00 -0400

    Screen target stocks and find point rating for each stock in different fiscal years

--- src/screener.py
+++ src/screener.py
@@ -12,6 +12,7 @@ financial_metrics = ['altman_z_score', 'gpm', 'pb_ratio', 'de_ratio', 'net_incom
 test_stocks = ['AAPL', 'TSLA', 'AMZN']
 software_ind = ['Software - Application', 'Internet Content & Information',]
 
+# Match companies with their respective industry
 ind_dict = {}
 for stock in test_stocks:
     ind = get_industry(ticker=stock)
@@ -21,13 +22,16 @@ years = pd.read_sql_query("""
     SELECT "Fiscal Years"
     FROM altman_z_score""", conn1)
 years = list(years['Fiscal Years'])
+years.reverse()
 
 def get_company_value(conn, metric, stock, year):
+    # Query financial metrics database to find a company's respective value
     query = f'SELECT {stock} FROM {metric} WHERE "Fiscal Years" = {year};'
     results  = pd.read_sql_query(query, conn).squeeze()
     return results
 
 def get_industry_value(conn, metric, industry):
+    # Query the industry averages db to find the average value for a company's industry
     query = f'SELECT {metric} from industries WHERE Industry = "{industry}"'
     results = pd.read_sql_query(query, conn).squeeze()
     return results
@@ -74,9 +78,26 @@ def compare_revenue_growth(comp_conn, industry_conn, metric, stock, year, indust
         return 1
     return 0
 
+# {'AAPL: {2021: 5, 2022: 3...}, 'TSLA: {2021; 3, 2022: 5...}}
+results_dict = {}
+inside_dict = {}
 for stock, industry in ind_dict.items():
     for year in years:
-
+        total_points = 0
+        z_score_points = compare_z_score(comp_conn=conn1, metric='altman_z_score', stock=stock, year=year)
+        total_points += z_score_points
+        gpm_points = compare_gpm(comp_conn=conn1, industry_conn=conn2, metric='gpm', stock=stock, year=year, industry=industry)
+        total_points += gpm_points
+        pb_points = compare_pb_ratio(comp_conn=conn1, industry_conn=conn2, metric='pb_ratio', stock=stock, year=year, industry=industry)
+        total_points += pb_points
+        de_points = compare_de_ratio(comp_conn=conn1, industry_conn=conn2, metric='de_ratio', stock=stock, year=year, industry=industry)
+        total_points += de_points
+        ni_growth_points = compare_net_income_growth(comp_conn=conn1, metric='net_income_growth', stock=stock, year=year)
+        total_points += ni_growth_points
+        rev_growth_points = compare_revenue_growth(comp_conn=conn1, industry_conn=conn2, metric='revenue_growth', stock=stock, year=year, industry=industry)
+        total_points += rev_growth_points
+        inside_dict[year] = total_points
+    results_dict[stock] = inside_dict
 
 conn1.close()
 conn2.close()
