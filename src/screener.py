056a82bbdc07167ed0ca837e4a73eedceba71f22
Justin <justinw86420@gmail.com>
2026-07-02 15:19:48 -0400

    Define comparison functions
    Each metric being compared was clearly defined in separate functions for easier readability

--- src/screener.py
+++ src/screener.py
@@ -17,15 +17,13 @@ for stock in test_stocks:
     ind = get_industry(ticker=stock)
     ind_dict[stock] = ind
 
-print(ind_dict)
-
 years = pd.read_sql_query("""
     SELECT "Fiscal Years"
     FROM altman_z_score""", conn1)
 years = list(years['Fiscal Years'])
 
-def get_company_value(conn, table, stock, year):
-    query = f'SELECT {stock} FROM {table} WHERE "Fiscal Years" = {year};'
+def get_company_value(conn, metric, stock, year):
+    query = f'SELECT {stock} FROM {metric} WHERE "Fiscal Years" = {year};'
     results  = pd.read_sql_query(query, conn).squeeze()
     return results
 
@@ -34,27 +32,50 @@ def get_industry_value(conn, metric, industry):
     results = pd.read_sql_query(query, conn).squeeze()
     return results
 
-def compare_z_score(company_value):
-    points = 0
-    z_score = company_value
-    if z_score >= 3:
-        points += 2
-    elif z_score >= 2:
-        points += 1
-    else:
-        points += 0
+def compare_z_score(comp_conn, metric, stock, year):
+    company_value = get_company_value(conn=comp_conn, metric=metric, stock=stock, year=year)
+    if company_value >= 3:
+        return 2
+    elif company_value >= 2:
+        return 1
+    return 0
 
-    return points
+def compare_gpm(comp_conn, industry_conn, metric, stock, year, industry):
+    company_value = get_company_value(conn=comp_conn, metric=metric, stock=stock, year=year)
+    industry_value = get_industry_value(conn=industry_conn, metric=metric, industry=industry)
+    if company_value >= industry_value:
+        return 1
+    return 0
 
-def compare_gpm(company_value, industry_value):
-    points = 0
-    company_gpm = company_value
-    industry_gpm = industry_value
-    if company_gpm > industry_gpm:
-        points += 1
-    else:
-        points += 0
-    return points
+def compare_pb_ratio(comp_conn, industry_conn, metric, stock, year, industry):
+    company_value = get_company_value(conn=comp_conn, metric=metric, stock=stock, year=year)
+    industry_value = get_industry_value(conn=industry_conn, metric=metric, industry=industry)
+    if company_value < industry_value:
+        return 1
+    return 0
+
+def compare_de_ratio(comp_conn, industry_conn, metric, stock, year, industry):
+    company_value = get_company_value(conn=comp_conn, metric=metric, stock=stock, year=year)
+    industry_value = get_industry_value(conn=industry_conn, metric=metric, industry=industry)
+    if company_value < industry_value:
+        return 1
+    return 0
+
+def compare_net_income_growth(comp_conn, metric, stock, year):
+    company_value = get_company_value(conn=comp_conn, metric=metric, stock=stock, year=year)
+    if company_value > 0:
+        return 1
+    return 0
+
+def compare_revenue_growth(comp_conn, industry_conn, metric, stock, year, industry):
+    company_value = get_company_value(conn=comp_conn, metric=metric, stock=stock, year=year)
+    industry_value = get_industry_value(conn=industry_conn, metric=metric, industry=industry)
+    if company_value >= industry_value:
+        return 1
+    return 0
+
+for stock, industry in ind_dict.items():
+    for year in years:
 
 
 conn1.close()
