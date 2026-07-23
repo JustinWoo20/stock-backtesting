3acd99d69060acf12734f88c80c904b5889e35f1
Justin <justinw86420@gmail.com>
2026-07-23 15:37:00 -0400

    Update date formatting

--- src/financial_metrics/fiscal_years.py
+++ src/financial_metrics/fiscal_years.py
@@ -1,7 +1,10 @@
+from datetime import date
+
 def get_fiscal_years(balance):
     fiscal_years = []
     for year in balance:
         fiscal_year = year['fiscalYear']
+        fiscal_year = int(fiscal_year)
         fiscal_years.append(fiscal_year)
 
     return fiscal_years
@@ -10,6 +13,7 @@ def get_filing_dates(balance):
     filing_dates = []
     for year in balance:
         filing_date = year['filingDate']
+        filing_date = date.strptime(filing_date, "%Y-%m-%d")
         filing_dates.append(filing_date)
 
     return filing_dates
